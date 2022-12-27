1ac0741d971e2d94e58175b0bb75da5d349cf786
hoangnguyennn <nvanhoang188@gmail.com>
2022-12-27 09:15:58 +0700

    add health check

---
+++ src/helpers/healthcheck.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import catcherWrapper from './catcherWrapper'
+import { success } from './commonResponse'
+
+const healthcheck = async (req: Request, res: Response) => {
+  const healthcheck = {
+    message: 'Server is running...',
+    uptime: process.uptime(),
+    timestamp: Date.now()
+  }
+
+  return success(res, healthcheck)
+}
+
+export default catcherWrapper(healthcheck)
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -2,11 +2,15 @@ import { Application, json, urlencoded } from 'express'
 import apiRoutes from '~/apis/routes'
 import env from '~/configs/env'
 import errorHandler from '~/helpers/errorHandler'
+import healthcheck from '~/helpers/healthcheck'
 
 export default async ({ app }: { app: Application }) => {
   // load middlewares
   app.use(json())
   app.use(urlencoded({ extended: true }))
+
+  // health check
+  app.get('/', healthcheck)
 
   // load routes
   app.use(env.apiPrefix, apiRoutes)
