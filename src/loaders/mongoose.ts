711fbdc5c4b98ec78ea1e5a790a7dcb61b85d4ad
hoangnguyennn <nvanhoang188@gmail.com>
2022-12-30 09:29:25 +0700

    update dependencies

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,6 +3,8 @@ import env from '~/configs/env'
 
 export default async () => {
   console.log('MongoDB is connecting...')
+  mongoose.set('strictQuery', true)
+  mongoose.set('debug', true)
   await mongoose.connect(env.mongoUri)
   console.log('MongoDB is connected')
 }
