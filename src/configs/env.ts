41a927b1b93f333b7c6c14ce4a728221855d6443
hoangnguyennn <nvanhoang188@gmail.com>
2024-07-31 21:08:39 +0700

    implement mail service

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -9,7 +9,11 @@ export default {
   port: Number(process.env.PORT),
   apiPrefix: String(process.env.API_PREFIX),
   mongoUri: String(process.env.MONGODB_URI),
-  maxOfSalt: Number(process.env.MAX_OF_SALT),
+  maxOfSalt: Number(process.env.TOKEN_MAX_OF_SALT),
   tokenSecret: String(process.env.TOKEN_SECRET),
-  tokenExpiresIn: String(process.env.TOKEN_EXPIRES_IN)
+  tokenExpiresIn: String(process.env.TOKEN_EXPIRES_IN),
+  mailHost: String(process.env.MAIL_HOST),
+  mailPort: Number(process.env.MAIL_PORT),
+  mailUser: String(process.env.MAIL_USER),
+  mailPassword: String(process.env.MAIL_PASSWORD)
 }
---
+++ src/services/mail.service.ts
@@ -0,0 +1,25 @@
+import { createTransport } from 'nodemailer'
+import Mail from 'nodemailer/lib/mailer'
+import config from '@hn/configs/env'
+
+const transport = createTransport({
+  host: config.mailHost,
+  port: config.mailPort,
+  auth: {
+    user: config.mailUser,
+    pass: config.mailPassword
+  }
+})
+
+const MailService = {
+  sendMail: async ({ from, to, subject, html }: Mail.Options) => {
+    return transport.sendMail({
+      from, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      html // html body
+    })
+  }
+}
+
+export default MailService
