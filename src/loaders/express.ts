ce562b593c06d989753fc706640c72f2868ccd63
hoangnguyennn <nvanhoang188@gmail.com>
2024-08-11 14:34:07 +0700

    remove api versioning

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -5,7 +5,6 @@ import apiRoutes from '@hn/apis/routes'
 import env from '@hn/configs/env'
 import errorHandler from '@hn/helpers/errorHandler'
 import healthcheck from '@hn/helpers/healthcheck'
-import apiV2Route from '@hn/v2/routes'
 
 export default async ({ app }: { app: Application }) => {
   // load middlewares
@@ -17,7 +16,6 @@ export default async ({ app }: { app: Application }) => {
 
   // load routes
   app.use(env.apiPrefix, apiRoutes)
-  app.use(`${env.apiPrefix}/v2`, apiV2Route)
 
   // celebrate error handler
   app.use(errors())
